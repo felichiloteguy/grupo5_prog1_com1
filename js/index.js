62bbd1e150b4f0582a89bbfc676f6ffbd20ae814
felichiloteguy <fchiloteguy@udesa.edu.ar>
2024-11-14 18:06:23 -0300

    haciendo pag principal

--- js/index.js
+++ js/index.js
@@ -4,6 +4,26 @@ fetch('https://dummyjson.com/recipes')
     })
     .then(function (data) {
         console.log(data);
+    
+        const menuRecetas= document.querySelector(".cuerpo");
+        
+        let recetas= "";
+        
+        for (let i = 0; i < data.recipes.length; i++) {
+            const receta= data.recipes[i];
+            recetas +=`
+            <article class="recetas">
+                 <img class= "image" src= "${receta.image}" alt="${receta.name}">
+                  <h1><a class= "titulo" href="./detalle.html?id=${receta.id}"> ${receta.name}</a></h1>
+                  <p> Tiempo de preparación: ${receta.prepTimeMinutes}</p>
+                  <p> Puntuación:  ${receta.rating}</p>
+            </article>
+            `;
+               
+        }
+        
+        
+        menuRecetas.innerHTML = recetas; 
         
     })
     .then(function (error) {
