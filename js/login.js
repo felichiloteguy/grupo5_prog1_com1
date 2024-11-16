e6d7c12567c1c447f9571f9569f3d0fb9aaa66d9
ClaraSchmidt1 <mschmidt@udesa.edu.ar>
2024-11-16 11:33:11 -0300

    Modificado HTML de login y creado JS para validaciones.

---
+++ js/login.js
@@ -0,0 +1,25 @@
+let form = document.querySelector(".contact-form");
+let errorEmail = document.querySelector(".email");
+let email = document.querySelector("#email");
+
+let errorPassword = document.querySelector(".password");
+let fullPassword = document.querySelector("#password");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let valida = true
+
+    if (email.value === "") {
+        alert("Por favor complete el campo email");
+        valida = false;
+    }
+    
+    if (password.value === "") {
+        alert("Por favor complete el campo contraseña");
+        valida = false;
+    }
+    
+    if(valida){
+        window.location.href = "home.html"
+    }
+})
