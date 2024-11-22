cb5d8abd972a0bbce8e7b5879715d85997053c1c
MailenHong <hongm@udesa.edu.ar>
2024-11-22 11:59:41 -0300

    en proceso del css y java del register

---
+++ js/register.js
@@ -0,0 +1,12 @@
+let form = document.querySelector('form-login form');
+let emailInput = document.querySelector('email');
+let errorEmail = document.querySelector ('#email')
+
+
+let passwordInput = document.querySelector('password');
+let errorPassword = document.querySelector('#password')
+
+let termsCheckbox = document.querySelector('terms');
+
+let submitButton = document.querySelector('.ingresar button');
+
