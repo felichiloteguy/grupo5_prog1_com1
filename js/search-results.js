25b14e5fef7b44b16382027d046cf924a9c475f5
felichiloteguy <fchiloteguy@udesa.edu.ar>
2024-11-22 10:50:43 -0300

    buscador

--- js/search-results.js
+++ js/search-results.js
@@ -1,28 +1,53 @@
 let queryString = location.search;
-let obj = new URLSearchParams(queryString)
-let palabraBuscada = obj.get("search")
+let obj = new URLSearchParams(queryString);
+let palabraBuscada = obj.get("search");
 console.log(palabraBuscada);
+
 fetch('https://dummyjson.com/recipes')
     .then(function (response) {
-        return response.json()
+        return response.json();
     })
     .then(function (data) {
         console.log(data);
-    
-        let result = document.querySelector(".result")
-        
-        result.addEventListener('#search', function(busqueda) {
-            busqueda.preventDefault();
-            if (palabraBuscada.value ) {
-                result.innerText = "No hay coincidencias con la búsqueda"
-            } else if (palabraBuscada.value) {
-                result.innerText = 'resultados de busqueda para: ${}'
+
+        let result = document.querySelector(".result");
+
+        if (!palabraBuscada) {
+            result.innerText = "No se ingresó ninguna búsqueda.";
+            return;
+        }
+
+        if (!data.recipes || data.recipes.length === 0) {
+            result.innerText = "No hay recetas disponibles.";
+        } else {
+            let resultados = [];
+            for (let i = 0; i < data.recipes.length; i++) {
+                let recipe = data.recipes[i];
+                
+                if (recipe.name.indexOf(palabraBuscada) !== -1) {
+                    resultados.push(recipe);
+                }
             }
-        })
-        
-       
+
+            if (resultados.length === 0) {
+                result.innerText = `No hay coincidencias con la búsqueda: "${palabraBuscada}"`;
+            } else {
+                result.innerHTML = `<h2>Resultados de búsqueda para: "${palabraBuscada}"</h2>`;
+                for (let i = 0; i < resultados.length; i++) {
+                    let recipe = resultados[i];
+                    result.innerHTML += `
+                        <div class="result-item">
+                            <img src="${recipe.image}" alt="${recipe.name}" />
+                            <h3>${recipe.name}</h3>
+                            <a href="./receta.html?id=${recipe.id}">Ver receta</a>
+                        </div>
+                    `;
+                }
+            }
+        }
     })
-    .then(function (error) {
-        console.log(`El error es:` + error);
-        
+    .catch(function (error) {
+        console.log(`El error es: ${error}`);
     });
+
+
